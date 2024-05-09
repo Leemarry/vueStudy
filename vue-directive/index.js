27715d3b52dfa387a0dc3c4934a9d117daac99ff
leeler <108058091+Leemarry@users.noreply.github.com>
2024-05-09 17:49:17 +0800

    Update index.js

--- vue-directive/index.js
+++ vue-directive/index.js
@@ -24,3 +24,46 @@ const directives = {  //汇总自定义指令
 //   useClickWindowDirective(app)
 //   //......
 // }
+
+// 如果是ts
+// #region
+
+import { App } from 'vue'
+import Focus from './Focus'
+
+
+const directivesList :any= {  //汇总自定义指令
+    Focus,//聚焦指令
+  }
+
+  
+const directives = {
+  install: function (app: App<Element>) {
+      Object.keys(directivesList).forEach(key => {
+          // 注册自定义指令
+          app.directive(key, directivesList[key]);
+      });
+  }
+};
+
+export default directives;
+// #endregion
+
+
+
+import type { Directive, DirectiveBinding } from "vue";
+
+interface ElType extends HTMLElement {
+  // 扩展了 HTMLElement 接口的属性
+  copyData: string | number;
+  __handleClick__: any;
+}
+
+const  Focus :Directive= {
+  mounted (el :ElType ,binding :DirectiveBinding) {
+    console.log('red', el)
+    el.style.background = 'yellow'
+  }
+}
+export default  Focus
+
